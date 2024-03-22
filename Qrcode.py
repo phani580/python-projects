7d022179626631777de2cfd3cac30135f4f8d03f
phani580 <130379106+phani580@users.noreply.github.com>
2024-03-22 20:28:08 +0530

    Add files via upload

---
+++ Qrcode.py
@@ -0,0 +1,30 @@
+import qrcode
+
+# taking UPI ID as a input
+upi_id =input("Enter your UPI ID = ")
+
+#upi://pay?pa=UPI_ID%apn=NAME&am=AMOUNT&cu=CURRENCY&tn=MESSAGE
+
+
+#Defining the payment URL based on the UPI ID and the Payment app
+#You can modify the URLs based on the payment apps you want to support
+
+phonepe_url=f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm_url=f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+google_pay_url=f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+
+phonepe_qr=qrcode.make(phonepe_url)
+paytm_qr=qrcode.make(paytm_url)
+google_pay_qr=qrcode.make(google_pay_url)
+
+#save the QR code to image file (optional)
+phonepe_qr.save('phonepe_qr.png')
+paytm_qr.save('paytm_qr.png')
+google_pay_qr.save('google_pay_qr.png')
+
+#Display the QR codes(you may need to install PIL/Pillow Library)
+
+phonepe_qr.show()
+paytm_qr.show()
+google_pay_qr.show()
